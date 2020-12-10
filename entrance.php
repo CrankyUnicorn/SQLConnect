243b4cb657de64072e322ab90ac2d407fbab1d41
CrankyUnicorn <azedo.peter@gmail.com>
2020-12-10 17:21:14 +0000

    New front images

--- entrance.php
+++ entrance.php
@@ -1,7 +1,7 @@
 	<div class="pc_forms pc_margin" style="text-align:center">
-		<h4>BEM-VINDO AO BACK-OFFICE DE</h4>
-		<h2>NAVEGANTES</h2>
-		<img src="images/leme.png" alt="Leme" width="400px">
-		
+		<h4 style="color:#222222;">BEM-VINDO AO BACK-OFFICE DE</h4>
+		<img src="images/logo_dark.png" alt="Leme" width="400px"><br>
+		<img src="images/leme_dark.png" alt="Leme" width="400px"><br><br>
+		<h5 style="color:#222222;">MARUJOS 2020</h5>
 	</div>
 
