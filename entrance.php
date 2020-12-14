77851bfdf48f4975b613e291654806a069cabc14
CrankyUnicorn <azedo.peter@gmail.com>
2020-12-14 23:32:36 +0000

    Late night commit - new inbetween classes created but not implemented

--- entrance.php
+++ entrance.php
@@ -1,7 +1,6 @@
 	<div class="pc_forms pc_margin" style="text-align:center">
-		<h4 style="color:#222222;">BEM-VINDO AO BACK-OFFICE DE</h4>
-		<img src="images/logo_dark.png" alt="Logo" width="400px"><br>
-		<img src="images/leme_dark.png" alt="Leme" width="400px"><br><br>
-		<h5 style="color:#222222;">MARUJOS 2020</h5>
+		<h3 style="color:#222222;">BEM-VINDO AO BACK-OFFICE DE</h3><br><br>
+		<img src="images/logo_mix_dark.png" alt="Logo" width="325px"><br>
+		<h5 style="color:#222222;">CrankyUnicorn 2020</h5>
 	</div>
 
--- htmldoc_start.php
+++ htmldoc_start.php
@@ -6,8 +6,8 @@
 	
   <title>SQLSERVER CONNECTION TEST</title>
   <meta name="description" content="">
-  <script type="text/javascript" src="script.js"></script>
-  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="scripts/script.js"></script>
+  <link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body id="pc_body">
@@ -17,10 +17,18 @@
   <img src="images/logo_white.png" alt="Logo" width="170px" style="padding: 0 0 8px 18px; opacity:60%;"><br>
   <div class="pc_marker pc_magenta"></div><a href="page_player.php">Jogadores</a>
   <div class="pc_marker pc_red"></div><a href="page_character.php">Personagens</a>
+  <div class="pc_marker pc_magenta_red"></div><a href="page_player_character.php">Personagens de Jogadores</a>
   <div class="pc_marker pc_orange"></div><a href="page_match.php">Partidas</a>
+  <div class="pc_marker pc_red_orange"></div><a href="page_character_match.php">Partidas de Personagens</a>
   <div class="pc_marker pc_yellow"></div><a href="page_vehicle.php">Veículos</a>
+  <div class="pc_marker pc_red_yellow"></div><a href="page_character_vehicle.php">Veículos de Personagens</a>
+  <div class="pc_marker pc_orange_yellow"></div><a href="page_match_vehicle.php">Veículos de Partidas</a>
   <div class="pc_marker pc_lightgreen"></div><a href="page_map.php">Mapa</a>
+  <div class="pc_marker pc_yellow_lightgreen"></div><a href="page_vehicle_map.php">Mapas dos Veículos</a>
   <div class="pc_marker pc_green"></div><a href="page_cell.php">Células</a>
+  <div class="pc_marker pc_lightgreen_green"></div><a href="page_map_cell.php">Células dos Mapas</a>
   <div class="pc_marker pc_teal"></div><a href="page_path.php">Caminhos</a>
+  <div class="pc_marker pc_red_teal"></div><a href="page_character_path.php">Caminhos das Personagens</a>
   <div class="pc_marker pc_lightblue"></div><a href="page_point.php">Pontos</a>
+  <div class="pc_marker pc_teal_lightblue"></div><a href="page_path_point.php">Pontos dos Caminhos</a>
 </div>
